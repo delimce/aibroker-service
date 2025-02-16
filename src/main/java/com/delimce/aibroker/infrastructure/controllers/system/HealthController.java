f8a8788039b709d1b30725a6e5bee3da21a2df02
delimce <delimce@gmail.com>
2025-02-16 09:44:17 +0100

    refactor: remove shared dir

--- src/main/java/com/delimce/aibroker/infrastructure/controllers/system/HealthController.java
+++ src/main/java/com/delimce/aibroker/infrastructure/controllers/system/HealthController.java
@@ -3,8 +3,8 @@ package com.delimce.aibroker.infrastructure.controllers.system;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.delimce.shared.domain.dto.ApiResponse;
-import com.delimce.shared.infrastructure.BaseController;
+import com.delimce.aibroker.domain.dto.ApiResponse;
+import com.delimce.aibroker.infrastructure.controllers.BaseController;
 
 @RestController
 final class HealthController extends BaseController {
--- src/main/java/com/delimce/shared/domain/exceptions/DomainException.java
+++
@@ -1,9 +0,0 @@
-package com.delimce.shared.domain.exceptions;
-
-public abstract class DomainException extends Throwable {
-
-    public DomainException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
