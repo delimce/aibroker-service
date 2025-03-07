d179d3fba1a67982026c4c3f51688b4902e7c222
ldelima <Glo.ldelimatovar@iberia.es>
2025-03-07 16:18:53 +0100

    chore: logging configuration and test

--- src/main/java/com/delimce/aibroker/infrastructure/controllers/system/CustomErrorController.java
+++ src/main/java/com/delimce/aibroker/infrastructure/controllers/system/CustomErrorController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.delimce.aibroker.domain.ports.LoggerInterface;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class CustomErrorController implements ErrorController {
+
+    private final LoggerInterface logger;
+
+    public CustomErrorController(LoggerInterface logger) {
+        this.logger = logger;
+    }
 
     @GetMapping("/error")
     public String handleError(HttpServletRequest request, HttpServletResponse response) {
@@ -22,6 +31,7 @@ public class CustomErrorController implements ErrorController {
             return "An error occurred. Please try again later.";
         } catch (Exception e) {
             response.setStatus(HttpStatus.NOT_FOUND.value());
+            logger.error("Exception in error handler: " + e.getMessage());
             return "404 - Resource not found.";
         }
     }
