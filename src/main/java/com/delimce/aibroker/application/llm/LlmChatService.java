5c75ecba163d69fa9ec275c02a421a2845737040
Luis D' Lima <delimce@gmail.com>
2025-05-18 22:33:20 +0200

    feat: implement user request tracking and metrics

--- src/main/java/com/delimce/aibroker/application/llm/LlmChatService.java
+++ src/main/java/com/delimce/aibroker/application/llm/LlmChatService.java
@@ -1,27 +1,37 @@
 package com.delimce.aibroker.application.llm;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.delimce.aibroker.domain.entities.Model;
+import com.delimce.aibroker.domain.entities.User;
+import com.delimce.aibroker.domain.entities.UserRequest;
 import com.delimce.aibroker.domain.ports.AiApiClientInterface;
 import com.delimce.aibroker.domain.repositories.ModelRepository;
+import com.delimce.aibroker.domain.repositories.UserRequestRepository;
 import com.delimce.aibroker.domain.dto.requests.llm.ModelRequest;
 import com.delimce.aibroker.domain.dto.responses.llm.ModelChatResponse;
+import com.delimce.aibroker.application.BaseService;
 
 @Service
-public class LlmChatService {
+public class LlmChatService extends BaseService {
 
     private final ModelRepository modelRepository;
+    private final UserRequestRepository userRequestRepository;
     private final AiApiClientInterface client;
 
-    public LlmChatService(ModelRepository modelRepository, AiApiClientInterface client) {
+    public LlmChatService(ModelRepository modelRepository, UserRequestRepository userRequestRepository,
+            AiApiClientInterface client) {
         this.modelRepository = modelRepository;
+        this.userRequestRepository = userRequestRepository;
         this.client = client;
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public ModelChatResponse execute(ModelRequest request) {
 
         Model model = modelRepository.findByName(request.getModel());
+
         if (model == null) {
             throw new IllegalArgumentException("Model not found");
         }
@@ -29,6 +39,18 @@ public class LlmChatService {
         if (!model.isEnabled()) {
             throw new IllegalArgumentException("Model is not enabled");
         }
+
+        User user = fetchAuthenticatedUser();
+
+        // save request data before send to model
+        UserRequest userRequest = UserRequest.builder()
+                .model(model)
+                .user(user)
+                .prompt(request.getMessages()[0].getContent())
+                .build();
+
+        // Save the user request to the database
+        userRequestRepository.save(userRequest);
 
         ModelChatResponse chatResponse = client.requestToModel(model, request);
 
