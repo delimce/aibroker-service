375bb0b6882a232bd931b606a9dd6a07447d7717
Luis D' Lima <delimce@gmail.com>
2025-05-30 17:58:59 +0200

    feat: add Flyway configuration and initial database migration scripts

--- src/main/java/com/delimce/aibroker/AibrokerApplication.java
+++ src/main/java/com/delimce/aibroker/AibrokerApplication.java
@@ -1,5 +1,6 @@
 package com.delimce.aibroker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,10 @@ import org.springframework.cache.annotation.EnableCaching;
 public class AibrokerApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(AibrokerApplication.class, args);
 	}
 
