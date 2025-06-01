79e8dfdd27e329045152b3f196f81e58537aa9cc
ldelima <ldelima@alfredsmart.com>
2025-06-02 00:16:49 +0200

    refactor: clean up UserListService and FlywayConfig by removing unused variables

--- src/main/java/com/delimce/aibroker/config/FlywayConfig.java
+++ src/main/java/com/delimce/aibroker/config/FlywayConfig.java
@@ -1,10 +1,8 @@
 package com.delimce.aibroker.config;
 
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
@@ -14,7 +12,6 @@ import javax.sql.DataSource;
 @Configuration
 public class FlywayConfig {
 
-    @Autowired
     /**
      * Creates and configures the Flyway instance.
      * 
